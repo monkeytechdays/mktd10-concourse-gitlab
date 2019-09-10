6bf1e47f969a05ee58d45d89bb6026b0114b7a34
Logan Mzz <loganmzz@users.noreply.github.com>
2019-09-10 14:12:31 +0200

    Adds Vault

--- mktd10/init/init.sh
+++ mktd10/init/init.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 function main() {
-    gogs
-    minio
+       gogs.main  \
+    && minio.main \
+    && vault.main
 }
 
-function wait_for_http() {
+function core.logs() {
+    local prefix="$1"; shift
+    sed "s/^/${prefix} /"
+}
+
+function http.wait_for() {
     local http_code=0
     local http_rc=1
     local http_attempt=4
@@ -23,23 +31,31 @@ function wait_for_http() {
     return 0
 }
 
-function gogs() {
-    echo "[GOGS] Wait for availability"
-    wait_for_http 'http://gogs:3000' || {
+function gogs.main() {
+    echo "[GOGS ]"
+    gogs_url='http://gogs:3000'
+    gogs.wait && gogs.install
+}
+
+function gogs.wait() {
+    echo "[GOGS ] Wait for availability"
+    http.wait_for "${gogs_url}" || {
         local rc=$?
-        echo "[GOGS] Not available" >&2
+        echo "[GOGS ] Not available" >&2
         return "$rc"
     }
+}
 
-    local installation_status=$(curl -L -s -o /dev/null -w '%{http_code}' -b '/tmp/gogs_install_cookies.txt' -c '/tmp/gogs_install_cookies.txt' 'http://gogs:3000/install')
-    echo "[GOGS] GET /install > ${installation_status}"
+function gogs.install() {
+    local installation_status=$(curl -L -s -o /dev/null -w '%{http_code}' -b '/tmp/gogs_install_cookies.txt' -c '/tmp/gogs_install_cookies.txt' "${gogs_url}/install") || return $?
+    echo "[GOGS ] GET /install > ${installation_status}"
 
-    [[ "${installation_status}" == '404' ]] || {
-        echo "[GOGS] POST /install"
-        curl -X POST -s -f 'http://gogs:3000/install' \
+    if [[ "${installation_status}" != '404' ]]; then
+        echo "[GOGS ] POST /install"
+        installation_status="$(curl -X POST -L -s "${gogs_url}/install" \
              -b '/tmp/gogs_install_cookies.txt' \
              -c '/tmp/gogs_install_cookies.txt' \
-             -o '/dev/null' \
+             -o '/tmp/gogs_install_post.txt' \
              -w '%{http_code}' \
              -F 'db_type=SQLite3' \
              -F 'db_host=127.0.0.1:3306' \
@@ -67,36 +83,115 @@ function gogs() {
              -F 'admin_passwd=root' \
              -F 'admin_confirm_passwd=root' \
              -F 'admin_email=root@localhost' \
-             || {
-                 local rc=$?
-                 echo "[GOGS] Installation done ($rc)"
-                 return $rc
-             }
-    }
-    echo "[GOGS] Installation already done"
+        )"
+        [[ "${installation_status}" -lt 300 ]] || {
+            echo "[GOGS ] Installation failed (${installation_status})" >&2
+            cat '/tmp/gogs_install_post.txt'
+            return 1
+        }
+        echo "[GOGS ] Installation done"
+    else
+        echo "[GOGS ] Installation already done"
+    fi
     return 0
 }
 
-function minio() {
-    local rc=0
+function minio.main() {
+    echo "[MINIO]"
+    minio.wait && minio.init_bucket
+}
 
+function minio.wait() {
     echo "[MINIO] Wait for availability"
-    wait_for_http -H 'User-Agent: Mozilla/5.0' 'http://s3:9000' || {
-        rc=$?
+    http.wait_for -H 'User-Agent: Mozilla/5.0' 'http://s3:9000' || {
+        local rc=$?
         echo "[MINIO] Not available" >&2
         return $rc
     }
+}
 
+function minio.init_bucket() {
     echo "[MINIO] Check bucket"
-    MC_HOST_s3="http://${MINIO_ACCESS_KEY}:${MINIO_SECRET_KEY}@s3:9000" mc ls -q s3/concourse || {
+    if ! MC_HOST_s3="http://${MINIO_ACCESS_KEY}:${MINIO_SECRET_KEY}@s3:9000" mc ls -q s3/concourse; then
         echo "[MINIO] Create bucket"
         MC_HOST_s3="http://${MINIO_ACCESS_KEY}:${MINIO_SECRET_KEY}@s3:9000" mc mb s3/concourse || {
+            local rc=$?
             echo "[MINIO] Unable to create bucket" >&2
-            return 1
+            return $rc
         }
-        return 0
+    else
+        echo "[MINIO] Bucket already exists"
+    fi
+    return 0
+}
+
+function vault.main() {
+    echo "[VAULT]"
+    vault.wait && vault.create_secrets && vault.create_policy && vault.create_token
+}
+
+function vault.wait() {
+    echo "[VAULT] Wait for availability"
+    http.wait_for "${VAULT_ADDR}" || {
+        local rc=$?
+        echo "[VAULT] Not available" >&2
+        return $rc
     }
-    echo "[MINIO] Bucket already exists"
+}
+
+function vault.create_secrets() {
+    echo "[VAULT] secrets: check /concourse"
+    local secrets_uuid="$(vault secrets list -format=json | jq -r '.["concourse/"]?.uuid')" || {
+        local rc=$?
+        echo "[VAULT] Unable to retrieve Vault secret list" >&2
+        return $?
+    }
+    if [[ "${secrets_uuid}" == 'null' ]]; then
+        echo "[VAULT] secrets: create /concourse"
+        vault secrets enable -path '/concourse' -description 'Concourse secrets' generic || {
+            local rc=$?
+            echo "[VAULT] Unable to create Vault secrets on '/concourse'" >&2
+            return $?
+        }
+    else
+        echo "[VAULT] secrets: /concourse already exists"
+    fi
+    return 0
+}
+
+function vault.create_policy() {
+    echo "[VAULT] policy: check 'policy-concourse'"
+    if ! vault policy list | grep -qx 'policy-concourse'; then
+        echo "[VAULT] policy: create 'policy-concourse'"
+        cat >/tmp/vault-policy-concourse.hcl <<EOF
+path "concourse/*" {
+    capabilities = ["read", "list"]
+}
+EOF
+        vault policy write 'policy-concourse' /tmp/vault-policy-concourse.hcl || {
+            local rc=$?
+            echo "[VAULT] Unable to create policy 'policy-concourse'" >&2
+            return $rc
+        }
+    else
+        echo "[VAULT] policy: 'policy-concourse' already exists"
+    fi
+    return 0
+}
+
+function vault.create_token() {
+    echo "[VAULT] token: check '${CONCOURSE_VAULT_CLIENT_TOKEN}'"
+    if ! vault token lookup "${CONCOURSE_VAULT_CLIENT_TOKEN}" >/dev/null 2>&1; then
+        echo "[VAULT] token: create '${CONCOURSE_VAULT_CLIENT_TOKEN}'"
+        vault token create -display-name 'Concourse token' -id "${CONCOURSE_VAULT_CLIENT_TOKEN}" -policy 'policy-concourse' || {
+            local rc=$?
+            echo "[VAULT] Unable to create token '${CONCOURSE_VAULT_CLIENT_TOKEN}'" >&2
+            return $rc
+        }
+    else
+        echo "[VAULT] token: '${CONCOURSE_VAULT_CLIENT_TOKEN}' already exists"
+    fi
+    return 0
 }
 
 main "$@#"
