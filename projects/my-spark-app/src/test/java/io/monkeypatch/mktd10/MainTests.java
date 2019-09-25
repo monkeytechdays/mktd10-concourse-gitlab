2a45b913d09369735757638de672a58dcc57e2ae
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2019-09-25 11:01:07 +0200

    Project exemples

---
+++ projects/my-spark-app/src/test/java/io/monkeypatch/mktd10/MainTests.java
@@ -0,0 +1,18 @@
+package io.monkeypatch.mktd10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTests {
+
+    @Test
+    void testFoo() {
+        assertEquals("foo", "foo");
+    }
+
+    @Test
+    void testBar() {
+        assertEquals("bar", "bar");
+    }
+}
